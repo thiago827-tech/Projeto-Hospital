fbdad1e3da665d02456995f768947107505fe989
Thiago Batista <thiago.batista41@etec.sp.gov.br>
2025-10-05 23:16:47 -0300

    Projeto Hospital C#

---
+++ Pessoa.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_CSharp
+{
+    internal class Pessoa
+    {
+        public string nome;
+        private string cpf;
+        private string sus;
+        public int idade;
+        public string endereco;
+        public string telefone;
+        public string email;
+
+
+
+
+        public void Cadastrar()
+        {
+            Console.Clear();
+            Console.WriteLine("Informe seu Nome:");
+            this.nome = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Informe seu CPF:");
+            this.cpf = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Informe o Numero do seu Cartao do SUS:");
+            this.sus = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Informe sua Idade:");
+            this.idade = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Informe seu Endereco:");
+            this.endereco = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Informe seu Telefone:");
+            this.telefone = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Informe seu Email:");
+            this.email = Console.ReadLine();
+        }
+
+        public void Consultar(string cpf)
+        {
+            if (string.IsNullOrWhiteSpace(cpf))
+            {
+                Console.Clear();
+                Console.WriteLine("Erro. Tente Novamente!");
+            }
+
+            if (cpf == this.cpf)
+            {
+                string resposta = "S";
+                while (resposta == "S")
+                {
+                    Console.Clear();
+                    Console.WriteLine("=================================================================");
+                    Console.WriteLine("Selecione o numero correspondente a alteracao que deseja realizar\n 1. Nome. \n 2. Idade. \n 3. Endereco. \n 4. Telefone. \n 5. Email. \n 6. Exibir Dados \n 0. Voltar pro Menu.");
+                    Console.WriteLine("=================================================================");
+                    string numero = Console.ReadLine();
+                    switch (numero)
+                    {
+                        case "1":
+                            Console.Clear();
+                            Console.WriteLine("Digite seu nome:");
+                            this.nome = Console.ReadLine();
+                            Console.Clear();
+                            Console.WriteLine("Deseja continuar alterações? (S/N)");
+                            resposta = Console.ReadLine();
+                            resposta = resposta.ToUpper();
+                            Console.Clear();
+                            if (resposta != "S" && resposta != "N")
+                            {
+                                Console.WriteLine("Opcao Invalida. Voltando ao Menu.");
+                                resposta = "N";
+                            }
+                            break;
+
+                        case "2":
+                            Console.Clear();
+                            Console.WriteLine("Digite sua Idade:");
+                            this.idade = int.Parse(Console.ReadLine());
+                            Console.Clear();
+                            Console.WriteLine("Deseja continuar alterações? (S/N)");
+                            resposta = Console.ReadLine();
+                            resposta = resposta.ToUpper();
+                            if (resposta != "S" && resposta != "N")
+                            {
+                                Console.WriteLine("Opcao Invalida. Voltando ao Menu.");
+                                resposta = "N";
+                            }
+                            break;
+
+                        case "3":
+                            Console.Clear();
+                            Console.WriteLine("Digite seu Endereco:");
+                            this.endereco = Console.ReadLine();
+                            Console.Clear();
+                            Console.WriteLine("Deseja continuar alterações? (S/N)");
+                            resposta = Console.ReadLine();
+                            resposta = resposta.ToUpper();
+                            if (resposta != "S" && resposta != "N")
+                            {
+                                Console.WriteLine("Opcao Invalida. Voltando ao Menu.");
+                                resposta = "N";
+                            }
+                            break;
+
+                        case "4":
+                            Console.Clear();
+                            Console.WriteLine("Digite seu Telefone:");
+                            this.telefone = Console.ReadLine();
+                            Console.Clear();
+                            Console.WriteLine("Deseja continuar alterações? (S/N)");
+                            resposta = Console.ReadLine();
+                            resposta = resposta.ToUpper();
+                            if (resposta != "S" && resposta != "N")
+                            {
+                                Console.WriteLine("Opcao Invalida. Voltando ao Menu.");
+                                resposta = "N";
+                            }
+                            break;
+
+                        case "5":
+                            Console.Clear();
+                            Console.WriteLine("Digite seu Email:");
+                            this.email = Console.ReadLine();
+                            Console.Clear();
+                            Console.WriteLine("Deseja continuar alterações? (S/N)");
+                            resposta = Console.ReadLine();
+                            resposta = resposta.ToUpper();
+                            Console.Clear();
+                            if (resposta != "S" && resposta != "N")
+                            {
+                                Console.WriteLine("Opcao Invalida. Voltando ao Menu.");
+                                resposta = "N";
+                            }
+                            break;
+
+                        case "6":
+                            Console.Clear();
+                            Console.WriteLine("|********** SEUS DADOS **********|\n");
+                            Console.WriteLine($"Nome: {this.nome}\nIdade: {this.idade}\nEndereco: {this.endereco}\nTelefone: {this.telefone}\nEmail: {this.email}");
+                            Console.WriteLine("\n\nDeseja continuar alterações? (S/N)");
+                            resposta = Console.ReadLine();
+                            resposta = resposta.ToUpper();
+                            Console.Clear();
+                            if (resposta != "S" && resposta != "N")
+                            {
+                                Console.WriteLine("Opcao Invalida. Voltando ao Menu.");
+                                resposta = "N";
+                            }
+                            break;
+
+                        case "0":
+                            Console.Clear();
+                            resposta = "N";
+                            break;
+
+                        default:
+                            Console.WriteLine("Opção Invalida.");
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu inicio = new Menu();
+            Pessoa[] array = new Pessoa[15];
+            int contador = 0;
+            bool continuar = true;
+
+            while (continuar)
+            {
+                switch (inicio.Hud())
+                {
+                    case "1":
+                        const int limite = 3;
+                        string res = "S";
+
+                        while (res == "S" && contador < limite)
+                        {
+                            Pessoa paciente = new Pessoa();
+                            paciente.Cadastrar();
+
+                            array[contador] = paciente;
+
+                            Console.Clear();
+                            Console.WriteLine("Paciente Cadastrado!!");
+                            Console.Clear();
+
+                            contador++;
+
+                            if (contador == limite)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Paciente Cadastrado!! \n\nLimite de Pacientes Excedido\n\n");
+                                Console.WriteLine("Pressione ENTER para voltar ao Menu...");
+                                Console.ReadLine();
+                                Console.Clear();
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("Deseja Cadastrar Outro Paciente? (S/N)");
+                                res = Console.ReadLine();
+                                res = res.ToUpper();
+                                Console.Clear();
+                                Console.WriteLine("Pressione ENTER para continuar...");
+                                Console.ReadLine();
+                                Console.Clear();
+                            }
+                            if (res != "S" && res != "N")
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Opção inválida. Voltando ao Menu.");
+                                res = "N";
+                                Console.Clear();
+                            }
+                        }
+                        break;
+
+                    case "2":
+                        Console.Clear();
+                        Console.WriteLine($"PACIENTES NA FILA ({contador + "/" + limite}):");
+                        if (contador == 0)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Não existem pacientes Cadastrados!\nPressione Enter para voltar ao Menu...");
+                            Console.ReadLine();
+                            Console.Clear();
+                        }
+                        else
+                        {
+                            for (int x = 0; x < contador; x++)
+                            {
+                                for (int j = 0; j < contador - 1; j++)
+                                {
+                                    if (array[j].idade < array[j + 1].idade)
+                                    {
+                                        Pessoa aux = array[j];
+                                        array[j] = array[j + 1];
+                                        array[j + 1] = aux;
+                                    }
+                                }
+                            }
+                            for (int y = 0; y < contador; y++)
+                            {
+                                Console.WriteLine($"Paciente {y + 1}: {array[y].nome}");
+                            }
+                            Console.WriteLine("\n\n\nPressione Enter para voltar ao Menu...");
+                            Console.ReadLine();
+                            Console.Clear();
+                        }
+                        break;
+
+                    case "3":
+                        Console.Clear();
+                        if (contador == 0)
+                        {
+                            Console.WriteLine("A fila de atendimento está vazia!\n\n\nPressione ENTER para retornar ao Menu.");
+                            Console.ReadLine();
+                            Console.Clear();
+                        }
+                        else
+                        {
+                            Pessoa pacienteAtendido = array[0];
+                            Console.WriteLine("|********** PACIENTE ATENDIDO **********|\n");
+                            Console.WriteLine($"O Paciente {pacienteAtendido.nome} de {pacienteAtendido.idade} anos, finalizou o atendimento!!\n\n");
+
+                            for (int x = 0; x < contador - 1; x++)
+                            {
+                                array[x] = array[x + 1];
+                            }
+                            array[contador - 1] = null;
+
+                            contador--;
+                            Console.WriteLine($"|LISTA DE PACIENTES NA FILA ATUALIZADA ({contador}/{limite})|");
+                            Console.WriteLine("Pressione ENTER para retornar ao Menu");
+                            Console.ReadLine();
+                        }
+                        break;
+
+                    case "4":
+                        Console.Clear();
+                        if (contador == 0)
+                        {
+                            Console.WriteLine("Não existe nenhum paciente cadastrado no sistema. Voltando ao Menu. \n\n\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Digite o CPF do paciente que deseja alterar dados cadastrais:");
+                            string cpfBusca = Console.ReadLine();
+                            for (int y = 0; y < contador; y++)
+                            {
+                                array[y].Consultar(cpfBusca);
+                            }
+                        }
+                        break;
+
+                    case "Q":
+                        Console.Clear();
+                        Console.WriteLine("Saindo do programa. Apert ESC para fechar.");
+                        Environment.Exit(1);
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Opção Invalida! Tente Novamente:");
+                        Console.WriteLine("Pressione ENTER para voltar ao Menu...");
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                }
+            }
+        }
+    }
+}
